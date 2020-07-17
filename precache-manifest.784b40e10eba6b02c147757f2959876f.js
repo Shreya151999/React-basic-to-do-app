711626a8fdd94805b05eaab09dd687c866854385
Shreya <60225158+Shreya151999@users.noreply.github.com>
2020-07-17 18:29:32 +0530

    Updates

---
+++ precache-manifest.784b40e10eba6b02c147757f2959876f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05edacaec6a928de43b142a52950df92",
+    "url": "/React-basic-to-do-app/index.html"
+  },
+  {
+    "revision": "ddbaef9f9a1822cf880e",
+    "url": "/React-basic-to-do-app/static/css/main.a47d2e57.chunk.css"
+  },
+  {
+    "revision": "58eabe3990788d22c5ff",
+    "url": "/React-basic-to-do-app/static/js/2.fafda29b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-basic-to-do-app/static/js/2.fafda29b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddbaef9f9a1822cf880e",
+    "url": "/React-basic-to-do-app/static/js/main.d01eeb40.chunk.js"
+  },
+  {
+    "revision": "753a1f6fd6ce64ee95cc",
+    "url": "/React-basic-to-do-app/static/js/runtime-main.d3429c89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-basic-to-do-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
